dec67fff5401caf6b78b80e64e6fdb48e6f19190
Raj <raaj18322@gmail.com>
2026-08-19 04:52:25 +0545

    Implement second solution for toLowerCase method
    Added a second solution for converting a string to lowercase using a StringBuilder.

--- easy/10_to_lower_case.java
+++ easy/10_to_lower_case.java
@@ -15,3 +15,27 @@ class Solution {
     }
 } */ 
 
+// second solution
+
+/*
+
+class Solution {
+    public String toLowerCase(String s) {
+        StringBuilder l = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char)(ch - 'A' + 'a');
+            }
+
+            l.append(ch);
+        }
+
+        return l.toString();
+    }
+}
+
+*/
+
