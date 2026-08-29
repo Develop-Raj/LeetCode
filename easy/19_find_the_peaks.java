649f7a157170ffd25425fcaa677ceafa617648c0
Raj <raaj18322@gmail.com>
2026-08-29 12:21:23 +0545

    Implement findPeaks method to identify peaks

---
+++ easy/19_find_the_peaks.java
@@ -0,0 +1,9 @@
+class Solution {
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> result = new ArrayList<Integer>();
+        for(int i = 1; i < mountain.length-1; i++){
+            if(mountain[i-1] < mountain[i] && mountain[i+1] < mountain[i] ) result.add(i);
+        }
+        return result;
+    }
+}
