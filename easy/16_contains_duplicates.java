17f0b4cda33f5867cca3bd6320ddb20db1423ba2
Raj <raaj18322@gmail.com>
2026-08-29 11:54:19 +0545

    Implement containsDuplicate method using HashSet

--- easy/16_contains_duplicates.java
+++ easy/16_contains_duplicates.java
@@ -11,3 +11,20 @@ class Solution {
 
 
 // 28ms - so i will try again
+
+
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int n : nums) {
+            if (!set.add(n)) return true;
+        }
+
+        return false;
+    }
+}
+
+
+// 13 ms
