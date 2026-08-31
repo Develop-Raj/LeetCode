f45534ee123bf3a4320a9dd727e800e54272101e
Raj <raaj18322@gmail.com>
2026-08-31 08:19:48 +0545

    Implement FizzBuzz solution in FizzBuzz.java

---
+++ easy/34 FizzBuzz.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> value = new ArrayList<String>();
+
+        for(int i = 1; i <= n; i++){
+            if(i % 3 == 0 && i % 5 == 0) value.add("FizzBuzz");
+            else if(i % 3 == 0) value.add("Fizz");
+            else if(i % 5 == 0) value.add("Buzz");
+            else value.add(String.valueOf(i));
+        }
+        return value;
+    }
+}
