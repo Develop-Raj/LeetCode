2fa480679c893603184e5740251f432ce1c2d63a
Raj <raaj18322@gmail.com>
2026-09-01 06:22:49 +0545

    Add solution for Best Time to Buy and Sell Stock

---
+++ easy/Best Time to Buy and Sell Stock.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int init = prices[0];
+        int maxProfit=0;
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < init){
+                init = prices[i];
+            }
+            else{
+                maxProfit = Math.max(maxProfit,prices[i]-init);
+            }
+        }
+        return maxProfit;
+    }
+}
